1a09f75b908ebe7722b235933e7ac6c1ee8bdc92
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-05-31 10:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+document.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
+
+document.addEventListener("drop", function(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  var draggedElement = document.getElementById(data);
+  var dropzone = event.target.closest('.image');
+
+  if (dropzone && dropzone !== draggedElement) {
+    var tempBackground = dropzone.style.backgroundImage;
+    dropzone.style.backgroundImage = draggedElement.style.backgroundImage;
+    draggedElement.style.backgroundImage = tempBackground;
+  }
+});
